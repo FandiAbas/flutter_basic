0fd54e1d937e69cb92f038038f7b3c7e27a1b542
FandiAbas <fandiabas00@gmail.com>
2024-06-06 11:27:42 +0800

    git init

---
+++ lib/mahasiswa.dart
@@ -0,0 +1,12 @@
+class Mahasiswa{
+  
+  int nim;
+  String name;
+  String address;
+
+  Mahasiswa({
+    required this.nim,
+    required this.name,
+    required this.address,
+  });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_hello/mahasiswa.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,62 +14,78 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData( 
+      title: 'flutter demo',
+      theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo kelas B'),
+       home: const MyHomePage(title: 'hello flutter kelas B'),
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  
-  
-  
-
-  
-  
-  
-  
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
+  
 class _MyHomePageState extends State<MyHomePage> {
+  bool isLoading=true;
+
+  List<Mahasiswa> listMahasiswa = [
+    Mahasiswa(nim: 123, name: 'fandi', address: 'bongomeme'),
+    Mahasiswa(nim: 456, name: 'ferdi', address: 'batudaa'),
+    Mahasiswa(nim: 789, name: 'mardi', address: 'marisa'),
+    Mahasiswa(nim: 543, name: 'aman', address: 'kampungjawa'),
+  ];
+
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        
-        
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        
         title: Text(widget.title),
       ),
+      
       body: ListView.builder(
-        padding:EdgeInsets.all(16),
-        itemBuilder: (context, index){
-        return Card(
+        padding: EdgeInsets.all(16),
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (_) {
+                    return AlertDialog(
+                      title: const Text('Data Mahasiswa'),
+                      content: Column(children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            const Text('Name : '),
+                            Text(listMahasiswa[index].name)
+                          ],
+                        ),
+                      ]),
+                    );
+                  });
+            },
+        
           child:ListTile(
-            leading: CircleAvatar(backgroundColor: Color.fromRGBO(235, 4, 4, 1),child: Text('$index'), 
+            leading: CircleAvatar(
+              backgroundColor: Color.fromRGBO(235, 4, 4, 1),
+              child: Text('${listMahasiswa[index].nim}'), 
           ),
-         title:Text('fandi'),
-          subtitle: Text('sedang belajar flutter'),
+         title:Text('${listMahasiswa[index].name}'),
+          subtitle: Text('${listMahasiswa[index].address}'),
         ),
         );
         },
-        itemCount: 20,
+        itemCount: listMahasiswa.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed:(){}, 
@@ -75,4 +94,5 @@ class _MyHomePageState extends State<MyHomePage> {
       ), 
     );
   }
+
 }
